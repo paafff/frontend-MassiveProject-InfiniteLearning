80f5f335833b10d2d906f5645cc66fe807c6ac6b
paafff <danang050402@gmail.com>
2023-11-19 21:34:35 +0700

    fix asmali

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
-import Layout from './Layout'
-
-
+import React from 'react';
+import Layout from './Layout';
+import LoginForm from '../components/LoginForm';
 
 const Login = () => {
   return (
     <Layout>
-        
+      <LoginForm />
     </Layout>
-  )
-}
-
-export default Login
+  );
+};
