8fb89527702605b1ec5069ad8e8cd41143e3c2c4
paafff <danang050402@gmail.com>
2023-11-19 20:31:28 +0700

    fix master

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,7 @@ import DashboardEmployee from './pages/User/DashboardEmployee';
 import DashboardBooking from './pages/User/DashboardBooking';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
-import DummyPaafff from './components/DummyPaafff';
-import DevPaafff from './pages/DevPaafff';
-import DummyPaafff from './components/DummyPaafff';
-import DevPaafff from './pages/DevPaafff';
+
 import BusinessRegistration from './pages/User/BusinessRegistration';
 import Reservasi from './pages/User/Reservasi';
 import AccountAdmin from './pages/Admin/AccountAdmin';
@@ -27,19 +24,21 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/sample" element={<Sample />} />
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/dashboard-bussiness" element={<DashboardBussiness />} />
+            <Route
+              path="/dashboard-bussiness"
+              element={<DashboardBussiness />}
+            />
             <Route path="/dashboard-employee" element={<DashboardEmployee />} />
             <Route path="/dashboard-booking" element={<DashboardBooking />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/dummy/paafff" element={<DummyPaafff />} />
-            <Route path="/dummy/paafff" element={<DevPaafff />} />
-            <Route path="/dummy/paafff" element={<DummyPaafff />} />
-            <Route path="/dummy/paafff" element={<DevPaafff />} />
-            <Route path="/BusinessRegistration" element={<BusinessRegistration />} />
-            <Route path="/Reservasi" element={<Reservasi/>} />
-            <Route path="/AccountAdmin" element={<AccountAdmin/>} />
-            <Route path="/InputReview" element={<InputReview/>} />
+            <Route
+              path="/BusinessRegistration"
+              element={<BusinessRegistration />}
+            />
+            <Route path="/Reservasi" element={<Reservasi />} />
+            <Route path="/AccountAdmin" element={<AccountAdmin />} />
+            <Route path="/InputReview" element={<InputReview />} />
           </Routes>
         </BrowserRouter>
       </div>
