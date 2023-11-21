17db3011c891f666486dd137ef922df0c0b54fcf
asmaliv <asmalijdzr@gmail.com>
2023-11-21 19:11:59 +0700

    Sign UP selesai

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,4 +1,10 @@
 import React, { useState } from 'react';
+import { FaAddressBook } from "react-icons/fa";
+import { MdMail } from "react-icons/md";
+import { FaUnlock } from "react-icons/fa";
+import { FaKey } from "react-icons/fa6";
+import Logosignup from "../assets/images/logo/signup.png"
+
 
 const SignUpForm = () => {
   const [fullName, setFullName] = useState('');
@@ -17,21 +23,27 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
+    <div className='bg-white w-full lg:p-8 xl:flex gap-10'>
+    <div className='md:w-1/2 md:mx-auto flex items-center xl:w-1/2 '>
+    <img src={Logosignup}></img>
+    </div>
+    <div className="bg-white w-full border-2 border-solid shadow-slate-300 py-5 px-10 xl:w-1/2">
       <form
-        className="bg-white shadow-xl w-full max-w-xs border-4 px-8 pt-6 pb-8 font-poppins"
+        className="flex flex-col gap-5"
         onSubmit={handleSubmit}
       >
-        <div className="mb-6">
+      <p className="text-base md:text-lg font-bold text-center">Sign Up</p>
+      <div className='flex flex-col gap-5 xl:flex-col xl:justify-start'>
+        <div className="flex flex-col gap-3 md:items-start md:justify-between">
           <label
-            className="block text-gray-700 text-sm font-bold mb-3"
+            className="text-sm font-semibold"
             htmlFor="username"
-          >
+          > <FaAddressBook className='inline-block mr-2 scale-125'/>
             Nama
           </label>
           <input
             type="text"
-            className="rounded w-full py-2 px-3 text-gray-700"
+            className="w-full  border border-gray-400 rounded-md h-10 text-sm font-semibold px-2 placeholder:text-xs disabled:bg-gray-200"
             id="username"
             placeholder="Nama Lengkap"
             value={fullName}
@@ -39,16 +51,16 @@ const SignUpForm = () => {
           />
         </div>
 
-        <div className="mb-6">
+        <div className="flex flex-col gap-2 md:items-start md:justify-between">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="text-sm font-semibold"
             for="email"
-          >
+          > <MdMail className='inline-block mr-2 scale-125'/>
             Email
           </label>
           <input
             type="text"
-            className="rounded w-full py-2 px-3 text-gray-700 "
+            className="w-full  border border-gray-400 rounded-md h-10 text-sm font-semibold px-2 placeholder:text-xs disabled:bg-gray-200"
             id="username"
             placeholder="name@gmail.com"
             value={email}
@@ -56,16 +68,16 @@ const SignUpForm = () => {
           />
         </div>
 
-        <div className="mb-6">
+        <div className="flex flex-col gap-2 md:items-start md:justify-between">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="text-sm font-semibold"
             for="password"
-          >
+          > <FaUnlock className='inline-block mr-2 scale-125'/>
             Password
           </label>
           <input
             type="password"
-            className="rounded w-full py-2 px-3 text-gray-700 mb-3 "
+            className="w-full  border border-gray-400 rounded-md h-10 text-sm font-semibold px-2 placeholder:text-xs disabled:bg-gray-200"
             id="password"
             placeholder="Minimal 8 Karakter"
             value={password}
@@ -73,35 +85,37 @@ const SignUpForm = () => {
           />
         </div>
 
-        <div className="mb-6">
+        <div className="flex flex-col gap-2 md:items-start md:justify-between">
           <label
-            className="block text-gray-700 text-sm font-bold mb-2"
+            className="text-sm font-semibold"
             for="password"
-          >
+          > <FaKey className='inline-block mr-2 scale-125'/>
             Ulangi Password
           </label>
           <input
             type="password"
-            className="rounded w-full py-2 px-3 text-gray-700 mb-3 "
+            className="w-full  border border-gray-400 rounded-md h-10 text-sm font-semibold px-2 placeholder:text-xs disabled:bg-gray-200"
             placeholder="Masukkan Kembali Password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </div>
 
-        <div>
-          <button classNameName="" type="submit">
+          <button className='w-full flex py-2 px-5 bg-red-400 hover:bg-red-400 hover:cursor-pointer transition-all rounded' type="submit">
+            <p className='text-base md:text-sm font-semibold text-center text-white w-full'>
             Sign Up
+            </p>
           </button>
-        </div>
 
-        <p className="mt-4 text-sm text-gray-600">
+        <p className="mt-2 text-sm font-semibold text-black text-center">
           Sudah punya akun HairHub?{' '}
-          <a href="#" className="text-blue-500">
+          <a className="text-red-400">
             Masuk di sini
           </a>
         </p>
+        </div>
       </form>
+      </div>
     </div>
   );
 };
